d2e97e89dc01a4f009fe8ca7809825cbbfeaef83
Marine <marine.magnin73@gmail.com>
2023-06-03 18:24:32 +0200

    Html et css : ajout du composant Banner + mise en page

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,16 @@
+import styles from './Banner.module.css'
+
+function Banner({ image, alt }) {
+    return (
+        <div className={styles.container}>
+            <div className={styles.containerImage}>
+                <img src={image} alt={alt} className={styles.image} />
+            </div>
+            <div className={styles.containerTitle}>
+                <h1 className={styles.title}>Chez vous,&nbsp;<span className={styles.span}>partout et ailleurs</span></h1>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
