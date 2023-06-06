df27c0bc0f23ba87c58152725df22b53040e57e1
Marine <marine.magnin73@gmail.com>
2023-06-06 09:20:31 +0200

    Jsx : modification code appel de render

--- src/index.jsx
+++ src/index.jsx
@@ -1,29 +1,27 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Logement from './pages/Logement';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Error from './components/Error';
+import styles from './Index.module.css';
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Logement from './pages/Logement'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Error from './components/Error'
-import styles from './Index.module.css'
-
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div className={styles.root}>
       <Router>
         <Header />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/apropos' element={<About />} />
-          <Route path='/logement/:id' element={<Logement />} />
-          <Route path='*' element={<Error />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/apropos" element={<About />} />
+          <Route path="/logement/:id" element={<Logement />} />
+          <Route path="*" element={<Error />} />
         </Routes>
         <Footer />
       </Router>
     </div>
-  </React.StrictMode >,
-  document.getElementById('root')
-)
+  </React.StrictMode>,
+);
