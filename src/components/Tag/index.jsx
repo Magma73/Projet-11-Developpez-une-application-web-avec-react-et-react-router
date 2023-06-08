aa0601706763c6020b897b7604fa14340c3c225f
Marine <marine.magnin73@gmail.com>
2023-06-08 19:29:05 +0200

    Html et css : ajout du composant Tag + mis en page

---
+++ src/components/Tag/index.jsx
@@ -0,0 +1,21 @@
+// import { useParams } from 'react-router-dom'
+// import { useEffect, useState } from 'react'
+import styles from './Tag.module.css'
+
+function Tag({ tags }) {
+
+    return (
+        <div className={styles.containerTags}>
+            {
+                tags.map((tag, index) => (
+                    < div className={styles.tag} key={index}>
+                        <p className={styles.text}>{tag}</p>
+                    </div >
+                ))
+            }
+        </div>
+    )
+
+}
+
+export default Tag
