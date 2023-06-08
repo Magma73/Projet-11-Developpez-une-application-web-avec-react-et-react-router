f1febc173e1788cf16744eabb52ca2984fb0e82e
Marine <marine.magnin73@gmail.com>
2023-06-08 19:29:50 +0200

    Html et css : ajout des composants à la page Logement + mise en page

--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -2,6 +2,9 @@ import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 // import Picture from '../../components/Picture'
 import Slideshow from '../../components/Slideshow'
+import Collapse from '../../components/Collapse'
+import Tag from '../../components/Tag'
+import Rating from '../../components/Rating'
 import styles from './Logement.module.css'
 
 function Logement() {
@@ -32,23 +35,57 @@ function Logement() {
 
     if (logementData) {
         const { title, pictures, host, description, rating, location, equipments, tags } = logementData;
+        const nameParts = host.name.split(' ');
+        const firstName = nameParts[0];
+        const lastName = nameParts[1];
 
         return (
-            <div className={styles.containerLogements}>
-                <div>
+            <div className={styles.containerMain}>
+                <div className={styles.containerSlide}>
                     <Slideshow pictures={pictures} title={title} />
                 </div>
 
-                {/* <Picture /> */}
-                <h1>{title}</h1>
-                <p>{host.name}</p>
-                <p>{equipments}</p>
-                <p>{description}</p>
-                <p>{rating}</p>
-                <p>{location}</p>
-                <p>{tags}</p>
-                {/* ... autres éléments de rendu */}
-            </div>
+                <div className={styles.containerInfos}>
+
+                    <div className={styles.containerTitleTagsProfilRating}>
+
+                        <div className={styles.containerTitleTags}>
+                            <div className={styles.containerTitle}>
+                                <h1 className={styles.title}>{title}</h1>
+                                <h2 className={styles.location}>{location}</h2>
+                            </div>
+                            <div className={styles.containerTags}>
+                                <Tag tags={tags} />
+                            </div>
+                        </div>
+
+                        <div className={styles.containerRatingProfil}>
+                            <div className={styles.containerRating}>
+                                <Rating rating={rating} />
+                            </div>
+                            <div className={styles.containerProfil}>
+                                <h3 className={styles.name} ><span>{firstName} </span><span>{lastName}</span></h3>
+                                <img className={styles.photo} src={host.picture} alt={host.name} />
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                    <div className={styles.containerDescriptionEquipments}>
+
+                        <div className={styles.containerDescription}>
+                            <Collapse label="Description" text={description} />
+                        </div>
+                        <div className={styles.containerEquipments}>
+                            <Collapse label="Équipements" text={equipments} />
+                        </div>
+
+                    </div>
+
+
+                </div >
+            </div >
         );
     } else {
         return <span>Logement introuvable</span>;
