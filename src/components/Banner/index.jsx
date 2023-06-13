6d2d6a54cca2394362069372f827680ce3d6371e
Marine <marine.magnin73@gmail.com>
2023-06-13 09:53:09 +0200

    Jsx : simplification code Banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -2,34 +2,23 @@ import styles from './Banner.module.css'
 import stylesAbout from './BannerAbout.module.css'
 
 function Banner({ image, alt, page }) {
+    let selectedStyles = page === 'about' ? stylesAbout : styles;
 
-    let selectedStyles = styles;
-
-    if (page === 'home') {
-        selectedStyles = styles;
-        return (
-            <div className={selectedStyles.container}>
-                <div className={selectedStyles.containerImage}>
-                    <img src={image} alt={alt} className={selectedStyles.image} />
-                </div>
+    return (
+        <div className={selectedStyles.container}>
+            {page === 'home' && (
                 <div className={selectedStyles.containerTitle}>
-                    <h1 className={selectedStyles.title}>Chez vous,&nbsp;<span className={selectedStyles.span}>partout et ailleurs</span></h1>
+                    <h1 className={selectedStyles.title}>
+                        Chez vous,&nbsp;
+                        <span className={selectedStyles.span}>partout et ailleurs</span>
+                    </h1>
                 </div>
+            )}
+            <div className={selectedStyles.containerImage}>
+                <img src={image} alt={alt} className={selectedStyles.image} />
             </div>
-        )
-    }
-
-    else if (page === 'about') {
-        selectedStyles = stylesAbout;
-        return (
-            <div className={selectedStyles.container}>
-                <div className={selectedStyles.containerImage}>
-                    <img src={image} alt={alt} className={selectedStyles.image} />
-                </div>
-            </div>
-        )
-    }
-
+        </div>
+    );
 }
 
 export default Banner
